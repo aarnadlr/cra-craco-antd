b1f8adac21048167b1a82c236c115d4a64596aa5
aarnadlr <aaronadler@gmail.com>
2019-11-30 15:12:47 -0500

    Revise to 'Daniel' voice. Minor styling.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,53 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 // import './App.css';
-import { Button } from 'antd';
+// import { Button } from 'antd';
 // import Speech from 'react-speech';
-import { useSpeechSynthesis } from "react-speech-kit";
+import { useSpeechSynthesis } from 'react-speech-kit';
 
 function App() {
-
-  const tts = `Hello and welcome to The Daily Review Podcast powered by the New York Times. I am your host, Max Adler.
-Today's top 35 stories from The New York Times, for your listening pleasure:`;
+  const tts = `Hello and welcome to The Daily Review Podcast. Today's top 35 stories from The New York Times:`;
 
   const [articles, setArticles] = useState(tts);
 
-  const [valToSpeak, setValToSpeak] = useState(tts);
+  // const [valToSpeak, setValToSpeak] = useState(tts);
+
+  const voiceArray = [
+    {
+      // default: true,
+      lang: 'en-AU',
+      localService: true,
+      name: 'Karen',
+      voiceURI: 'Karen'
+    },
+    {
+      voiceURI: 'Google US English',
+      name: 'Google US English',
+      lang: 'en-US',
+      localService: false,
+      default: true
+    }
+  ];
+
+  // VOICE 1!!!
+  const [voiceIndex, setVoiceIndex] = useState(7);
 
   const { speak, cancel, voices } = useSpeechSynthesis();
 
+  // VOICE 2!!
+  const voice = voices[voiceIndex] || null;
+
+  // const onEnd = () => {
+  //   // You could do something here after speaking has finished
+  // };
+
 
   const reducer = (string, currObj) => {
-
     let title = currObj.title;
     let abstract = currObj.abstract;
 
-    let finalString = string.concat(' ', title).concat(' ', abstract);
+    let finalString = string.concat(' ', title).concat('. ', abstract);
 
     return finalString;
-
   };
 
   useEffect(() => {
@@ -44,36 +67,64 @@ Today's top 35 stories from The New York Times, for your listening pleasure:`;
         // results is an ARR of OBJECTS
         // data.results.reduce(reducer, "")
 
-        setArticles(
-          data.results.reduce(reducer, "")
-        )
-
-      })
-      // .then(()=>{
-      //   return speak({ text: valToSpeak })
-      // });
-
-  }, []);
+        setArticles(tts.concat(' ', data.results.reduce(reducer, '')));
+      });
+    // .then(()=>{
+    //   return speak({ text: valToSpeak })
+    // });
+  }, [tts]);
 
   return (
-    <div className="App">
+    <div className="App" style={{display:'flex', flexDirection:'column', margin:'32px'}}>
+
+
+      {
+        voices.forEach((item, index)=>{
+          console.log(`VOICE ${index}: `, item)
+        })
+      }
+
+      {/*{console.log('VOICES:', voices)}*/}
+
+
       <h1>The Daily Review Podcast</h1>
-      {/*<h2>Top Stories</h2>*/}
-      {/*<h3>Total number of articles below: {articles && articles.length}</h3>*/}
 
-      Hello and welcome to The Daily Review Podcast powered by the New York
-      Times. I am your host, Max Adler.
-      <br />
-      Today's top {articles && articles.length} stories from The New York Times,
-      for your listening pleasure:
+
       <br />
 
-      <button style={{background:'limegreen', width:'240px', padding:'24px', margin:'16px', borderRadius:'32px'}} type={'primary'} onClick={() => speak({ text: articles })}>▶️ PLAY The Daily Review</button>
-      <br/>
-      <br/>
-      <button style={{background:'red', width:'240px', padding:'8px 24px', margin:'16px', borderRadius:'32px'}} type={'primary'} onClick={() => cancel()}>STOP</button>
-      <br/>
-      <br/>
+
+      <button
+        style={{
+          background: 'limegreen',
+          width: '240px',
+          padding: '24px',
+          margin: '16px',
+          borderRadius: '32px'
+        }}
+        type={'primary'}
+        onClick={() => speak({ text: articles, voice })}
+      >
+        ▶️ PLAY The Daily Review
+      </button>
+
+      <br />
+
+      <button
+        style={{
+          background: 'red',
+          width: '240px',
+          padding: '8px 24px',
+          margin: '16px',
+          borderRadius: '32px'
+        }}
+        type={'primary'}
+        onClick={() => cancel()}
+      >
+        STOP
+      </button>
+      <br />
+      <br />
+
       {/*{articles &&*/}
       {/*  articles.map(({ section, title, abstract, published_date }, index) => (*/}
       {/*    <p key={index}>*/}
@@ -83,7 +134,9 @@ Today's top 35 stories from The New York Times, for your listening pleasure:`;
       {/*    </p>*/}
       {/*  ))}*/}
 
-        {articles}
+      <h3>Transcript:</h3>
+
+      <p>{articles}</p>
 
     </div>
   );
