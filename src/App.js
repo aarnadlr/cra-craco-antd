836a581107a9e44efc1fead07efa8378e93c0ced
aarnadlr <aaronadler@gmail.com>
2019-12-01 12:59:26 -0500

    Use reduce to build up a long string containing: intro, story number, title, abstract.

--- src/App.js
+++ src/App.js
@@ -1,97 +1,75 @@
 import React, { useEffect, useState } from 'react';
-// import './App.css';
-// import { Button } from 'antd';
-// import Speech from 'react-speech';
 import { useSpeechSynthesis } from 'react-speech-kit';
 
 function App() {
-  const tts = `Hello and welcome to The Daily Review Podcast. Today's top 35 stories from The New York Times:`;
 
-  const [articles, setArticles] = useState(tts);
+  const [totalNumOfArticles, setTotalNumOfArticles] = useState(null);
 
-  // const [valToSpeak, setValToSpeak] = useState(tts);
 
-  const voiceArray = [
-    {
-      // default: true,
-      lang: 'en-AU',
-      localService: true,
-      name: 'Karen',
-      voiceURI: 'Karen'
-    },
-    {
-      voiceURI: 'Google US English',
-      name: 'Google US English',
-      lang: 'en-US',
-      localService: false,
-      default: true
-    }
-  ];
+  const [longTranscriptString, setLongTranscriptString] = useState('Loading...');
 
-  // VOICE 1!!!
-  const [voiceIndex, setVoiceIndex] = useState(7);
+  // SELECT WHICH VOICE by index
+  const [voiceIndex] = useState(7);
 
   const { speak, cancel, voices } = useSpeechSynthesis();
 
-  // VOICE 2!!
+  // SELECTED VOICE
   const voice = voices[voiceIndex] || null;
 
-  // const onEnd = () => {
-  //   // You could do something here after speaking has finished
-  // };
-
-
-  const reducer = (string, currObj) => {
-    let title = currObj.title;
-    let abstract = currObj.abstract;
-
-    let finalString = string.concat(' ', title).concat('. ', abstract);
-
-    return finalString;
-  };
 
   useEffect(() => {
+
+    let finalSingleString = '';
+
     fetch(
       'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=YFNhGhkzEe0Yq3KwV25z5b7qNvzGYmne'
     )
       .then(res => res.json())
       .then(data => {
-        // setArticles(data.results)
 
-        // setArticles(
-        //   data.results.filter(articleObj => {
-        //     return articleObj.section !== 'Opinion';
-        //   })
-        // );
+        // Save the total num of articles (to render separately)
+        setTotalNumOfArticles(data.results.length);
 
-        // results is an ARR of OBJECTS
-        // data.results.reduce(reducer, "")
+        let podcastIntroString = `Hello and welcome to The Daily Review Podcast. Today's top ${data.results.length} stories from The New York Times:`;
 
-        setArticles(tts.concat(' ', data.results.reduce(reducer, '')));
-      });
-    // .then(()=>{
-    //   return speak({ text: valToSpeak })
-    // });
-  }, [tts]);
+        // Store the long final accum string
+        finalSingleString =
+          data.results.reduce(
+            (accumString, currObj, ind, srcArr) => {
+              let prefix = `Story number ${ind + 1}.`;
+
+              let title = `${currObj.title}.`;
+              let abstract = currObj.abstract;
+
+              //Build the single string for each articleObj:
+              // concat the prefix, the title, the abstract
+              let singleString = prefix.concat(' ', title).concat(' ', abstract);
+
+              // Add to the accumString the single string
+              return accumString.concat(' ', singleString);
+            }, ''
+          );
+        let introWithLongString = podcastIntroString.concat(finalSingleString);
+        //Take the finalSingleString and save it to state
+        setLongTranscriptString(introWithLongString);
+      });  //<--END OF THEN BLOCK
+
+  }, []);
 
   return (
-    <div className="App" style={{display:'flex', flexDirection:'column', margin:'32px'}}>
-
-
-      {
-        voices.forEach((item, index)=>{
-          console.log(`VOICE ${index}: `, item)
-        })
-      }
-
-      {/*{console.log('VOICES:', voices)}*/}
-
+    <div
+      className="App"
+      style={{ display: 'flex', flexDirection: 'column', margin: '32px' }}
+    >
+      {/*{voices.forEach((item, index) => {*/}
+      {/*  console.log(`VOICE ${index}: `, item);*/}
+      {/*})}*/}
 
       <h1>The Daily Review Podcast</h1>
 
+      <h3>(Total num of articles: {totalNumOfArticles})</h3>
 
       <br />
-
 
       <button
         style={{
@@ -102,9 +80,9 @@ function App() {
           borderRadius: '32px'
         }}
         type={'primary'}
-        onClick={() => speak({ text: articles, voice })}
+        onClick={() => speak({ text: longTranscriptString, voice })}
       >
-        ▶️ PLAY The Daily Review
+        <span role="img" >▶</span>️ PLAY The Daily Review
       </button>
 
       <br />
@@ -122,22 +100,13 @@ function App() {
       >
         STOP
       </button>
-      <br />
-      <br />
 
-      {/*{articles &&*/}
-      {/*  articles.map(({ section, title, abstract, published_date }, index) => (*/}
-      {/*    <p key={index}>*/}
-      {/*      {title}.*/}
-      {/*      {abstract}*/}
-      {/*      /!*{' '}Next article:*!/*/}
-      {/*    </p>*/}
-      {/*  ))}*/}
+      <br />
+      <br />
 
       <h3>Transcript:</h3>
 
-      <p>{articles}</p>
-
+      <p>{longTranscriptString}</p>
     </div>
   );
 }
